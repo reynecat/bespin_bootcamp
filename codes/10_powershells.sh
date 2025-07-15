02e7a21b200a41d5fd59a16dae692d3d375180ce
reynecat <reyne7055@gmail.com>
2025-07-15 03:13:34 +0000

    fourth commit

---
+++ codes/10_powershells.sh
@@ -0,0 +1,40 @@
+#
+PS C:\Users\Administrator> ls
+디렉터리: C:\Users\Administrator
+
+Mode                 LastWriteTime         Length Name
+----                 -------------         ------ ----
+d-----      2025-07-14   오후 2:27                .docker
+d-----      2025-06-11   오후 5:48                .ms-ad
+d-----      2024-12-09  오전 10:40                .thumbnails
+d-----      2025-07-14  오전 11:56                .vscode
+d-r---      2025-06-11   오후 5:43                3D Objects
+d-r---      2025-06-11   오후 5:43                Contacts
+d-r---      2025-07-15  오전 11:04                Desktop
+d-r---      2025-07-14   오후 2:40                Documents
+d-r---      2025-07-15  오전 11:04                Downloads
+d-r---      2025-06-11   오후 5:43                Favorites
+d-r---      2025-06-11   오후 5:43                Links
+d-r---      2025-06-11   오후 5:43                Music
+d-r---      2023-11-07  오전 10:00                OneDrive
+d-r---      2025-07-14   오후 2:34                Pictures
+d-r---      2025-06-11   오후 5:43                Saved Games
+d-r---      2025-06-11   오후 5:43                Searches
+d-r---      2025-06-13   오전 9:41                Videos
+
+PS C:\Users\Administrator> cd c:\
+PS C:\> cd Develops
+PS C:\Develops> ls
+
+디렉터리: C:\Develops
+
+Mode                 LastWriteTime         Length Name
+----                 -------------         ------ ----
+d-----      2025-07-14   오후 5:19                codes
+d-----      2025-07-14  오후 12:00                Downloads
+
+PS C:\Develops> pwd
+
+Path
+----
+C:\Develops
